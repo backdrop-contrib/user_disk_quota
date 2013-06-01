471496c2595041929acaa810e69c400500c7e988
unknown <kongoji@gmail.com>
2013-06-01 19:32:46 +0200

    Added validation for settings page

--- js/user_disk_quota-admin.js
+++ js/user_disk_quota-admin.js
@@ -10,9 +10,11 @@
           $( ".user-disk-percentage-slider" ).val( ui.value );
         }
       });
-      $( ".user-disk-percentage-slider" ).change(function(){
-        //TODO check if numeric and in 0/100 range
-        $( ".user-disk-percentage-slider-range-max" ).slider( "option", "value", $(this).val() );
+      $( ".user-disk-percentage-slider" ).keyup(function(){
+        var value = parseInt($(this).val(), 10);
+        if (!isNaN(value) && value>=0 && value<=100) {
+          $( ".user-disk-percentage-slider-range-max" ).slider( "option", "value", value);
+        }
       });
     }
   };
